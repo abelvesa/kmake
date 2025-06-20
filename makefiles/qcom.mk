c6c8fd6dfc0cdd07e7343e9bf1f8a0e12fae1db0
Abel Vesa <abel.vesa@linaro.org>
2025-06-20 15:35:34 +0300

    Qualcomm X Elite CRD: Add config, build env and target

--- makefiles/qcom.mk
+++ makefiles/qcom.mk
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0
 
+PHONY += config/qcom/x1e80100/crd
+config/qcom/x1e80100/crd:
+	$(call do_config,qcom,x1e80100,crd)
+
 PHONY += config/qcom/x1e80100/devkit
 config/qcom/x1e80100/devkit:
 	$(call do_config,qcom,x1e80100,devkit)
